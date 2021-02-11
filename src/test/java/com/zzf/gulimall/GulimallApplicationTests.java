2d9bd7a135cec1694a43ef4095c47e931dce00b0
302600 <1397163661@qq.com>
2021-02-11 18:47:55 +0800

    test

---
+++ src/test/java/com/zzf/gulimall/GulimallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzf.gulimall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
